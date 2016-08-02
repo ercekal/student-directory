8e0836ca1ffe7c108240935fdf7ffcb9fcd154c3
ercekal <ercekalabalikoglu@gmail.com>
2016-08-02 23:52:46 +0100

    methods are created for all

--- directory.rb
+++ directory.rb
@@ -13,11 +13,21 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"]
 
+def print_header
 puts "The students of Villains Academy"
 puts "----------------"
-students.each do |student|
-  puts student
 end
 
+def print(names)
+  names.each do |name|
+  puts name
+end
+end
 
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
