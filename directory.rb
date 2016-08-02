50e228ca424afe91c445aeb33d8a1076c07b83d9
ercekal <ercekalabalikoglu@gmail.com>
2016-08-02 23:36:00 +0100

    Print part is removed

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villains Academy"
 puts "----------------"
 puts "Dr. Hannibal Lected"
@@ -12,8 +14,5 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
-print "Overall, we have "
-print 11
-puts " great students"
-
- 
+puts "Overall, we have #{student_count
+} great students"
