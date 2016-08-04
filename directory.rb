ecae76a43a1f6e4f42830128e202be8cec6ca275
ercekal <ercekalabalikoglu@gmail.com>
2016-08-05 00:40:10 +0100

    14.1 DRY

--- directory.rb
+++ directory.rb
@@ -1,43 +1,95 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
+  cohort = :november
   # while the name is not empty, repeat this code
   while !name.empty? do
-  # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-  # get another name from the user
-  name = gets.chomp
+    # add the student hash to the array
+    input_load(name, cohort)
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def input_load(name, cohort)
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have " + students.count.to_s + " great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-
-students = input_students
-if students.count >= 1
-print_header
-print(students)
-print_footer(students)
-else
-  puts "There is no student"
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    input_load(name, cohort)
+  end
+  file.close
+end
+
+interactive_menu
