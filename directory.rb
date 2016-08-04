445851ba48493700d8b8e13892a5f5c2a5561894
ercekal <ercekalabalikoglu@gmail.com>
2016-08-04 15:51:26 +0100

    interactive menu and save

--- directory.rb
+++ directory.rb
@@ -1,30 +1,53 @@
+@students = [] # an empty array accessible to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-students = []
-
-  puts "Please enter the name"
+  # get the first name
   name = gets.chomp
-    puts "Please enter the country of birth"
-  cob = gets.chomp
-    puts "Please enter the hobby"
-  hobby = gets.chomp
-    puts "Please enter the height"
-  height = gets.chomp
-    while !name.empty? || !cob.empty? || !hobby.empty? || !height.empty? do
-    students << {name: name, cob: cob, hobby: hobby, height: height, cohort: :november}
-    puts "Now we have #{students.count} students"
-    puts "Please enter the name"
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
-      puts "Please enter the country of birth"
-    cob = gets.chomp
-      puts "Please enter the hobby"
-    hobby = gets.chomp
-      puts "Please enter the height"
-    height = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -32,53 +55,24 @@ def print_header
   puts "-------------"
 end
 
-def student_by_first_char(students)
-  students.each do |student|
-    letter = 'c'
-    if student[:name][0] == letter
-      puts "The students starting with " +letter + " are listed below"
-      puts "#{student[:name]}"
-    end
-  end
-end
-
-def students_names_less_than_12_chars(students)
-        puts "Students with short names (<12) are listed below"
-  students.each do |student|
-    if student[:name].length < 12
-      puts "#{student[:name]}"
-    end
-  end
-end
-
-
-def print(students)
-  i = 0
-  until i == students.count
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
-    i = i + 1
-  end
-end
-
-
-def print_index(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_index(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts = csv_line
   end
+  file.close
 end
 
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
